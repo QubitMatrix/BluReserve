fe5a3dd378c192d2524828ae3b6b859a2f69a16f
QubitMatrix <preethi13m@gmail.com>
2025-01-30 23:26:49 +0530

    created reservation microservice

---
+++ reservation_service.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from seat_service import Seat
+from sqlalchemy import func
+from datetime import datetime
+from models import db, Seat, Employee
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///seat1.db'
+db.init_app(app)
+
+@app.route('/reserve', methods=['POST'])
+def reserve_seat():
+    data = request.get_json()
+    seat_id = data['seat_id']
+    start_time = data['start_time']
+    start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M")
+    end_time = data['end_time']
+    end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M")
+    emp_id = data['emp_id']
+    booked_seat = Seat(seat_id=seat_id, booked_by=emp_id, booking_start_time=start_time, booking_end_time=end_time)
+    db.session.add(booked_seat)
+    db.session.commit()
+    return jsonify({"message":"Booking successful"}), 201
+
+@app.route('/view_reserved', methods=['POST'])
+def get_reserved_seats():
+    data = request.get_json()
+    emp_id = data['emp_id']
+    seats = Seat.query.filter_by(booked_by=emp_id).all()
+    seats_arr = []
+    for seat in seats:
+        seats_arr.append(seat.to_dict())
+    print(seats_arr)
+    if(seats_arr):
+        return jsonify({"reserved_seats": seats_arr}), 200
+    else:
+        return jsonify({"message": "no seats reserved"})
+
+@app.route('/cancel_reservation', methods=['POST'])
+def cancel_reservation():
+    data = request.get_json()
+    emp_id = data['emp_id']
+    seat_id = data['seat_id']
+    start_time = data['start_time']
+    start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M")
+    end_time = data['end_time']
+    end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M")
+    seat = Seat.query.filter_by(seat_id=seat_id, booking_start_time=start_time, booking_end_time=end_time).first()
+    if seat:
+        seat.booked_by = None
+        seat.booking_start_time=None
+        seat.booking_end_time = None
+        db.session.commit()
+        return jsonify({"message": "Seat canceled successfully"})
+    else:
+        return jsonify({"message": "Seat not found"})
+    
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=5003)
